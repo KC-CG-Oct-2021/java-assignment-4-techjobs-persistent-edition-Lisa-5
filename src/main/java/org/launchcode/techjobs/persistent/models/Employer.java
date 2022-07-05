394b2a1fa3c3097307263cf6a06f221b3c84095c
Lisa Ulricksen <lisa.joshua1.9@gmail.com>
2022-07-05 17:14:45 -0500

    all but one test in queries.sql passing, app still not working as expected yet

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,31 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 255, message = "Location required")
+    private String location;
 
+   // @OneToMany(mappedBy = "employer")
+    @OneToMany @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
